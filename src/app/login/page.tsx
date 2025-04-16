757f42f4b3d61a5d5ecaa1b5a407b979bdc00b6c
Nadruz <juliananadruz@gmail.com>
2025-04-15 21:31:50 -0300

    feat: Create login panel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,29 @@
 
-export default function Login(){
-    return (
-        <div className="div">
-            
-        </div>
+import styles from "./login.module.css";
 
-        
-    )
+export default function Login() {
+    return (
+        <div className={styles.global}>
+            <div className={styles.screen1}>
+                <img src="logo" alt="Logo" className={styles.logoImg} />
+                <h1 className={styles.logoTitle}>avaHelper</h1>
+                <img src="" alt="" className={styles.logoImg} />
+            </div>
+            <div className={styles.screen2}>
+                <h2 className={styles.title}>Login</h2>
+
+                <form className={styles.form}>
+                    <label htmlFor="email" className={styles.label}>E-mail:</label>
+                    <input type="email" id="email" name="email" required className={styles.input} />
+
+                    <label htmlFor="password" className={styles.label}>Senha:</label>
+                    <input type="password" id="password" name="password" required className={styles.input} />
+
+                    <a href="#" className={styles.forgotLink}>Esqueceu a senha?</a>
+
+                    <input type="submit" value="Enviar" className={styles.submitButton} />
+                </form>
+            </div>
+        </div>
+    );
 }
