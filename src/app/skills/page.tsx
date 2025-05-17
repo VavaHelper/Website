d28060fa557a26fca54c711705faeba4a7ba1965
IGR-cK <117829729+IGR-cK@users.noreply.github.com>
2025-05-16 23:31:17 -0300

    feat(skills): Update skills page and module css.

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,9 +1,25 @@
 // TODO: Igão e Matheus -> Iniciar tela de Skills
 
+import styles from './skills.module.css'
+import { Nav } from '../components/nav';
+import { SideBar } from '../components/side-bar';
+
 export default function Skills() {
     return (
-
-        <h1>Skills</h1>
-
-        );
+        <div className={styles.global}>
+            <Nav/>
+            <SideBar/>
+            <div className={styles.containersearch}>
+                <h1>Agents</h1>
+                <p className={styles.description}>Explore agent abilities, lineups and strategies</p>
+                <div className={styles.filter}>
+                    <button className={styles.button} type="button">All</button>
+                    <button className={styles.button} type="button">Duelists</button>
+                    <button className={styles.button} type="button">Initiators</button>
+                    <button className={styles.button} type="button">Controllers</button>
+                    <button className={styles.button} type="button">Sentinels</button>
+                </div>
+            </div>
+        </div>
+    );
 }
