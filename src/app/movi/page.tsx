54c32c74eae3182fa26b7c62b03a342b8f053a41
LunaF11 <Lunasantana120@gmail.com>
2025-05-22 14:01:58 -0300

    updating the movement page

--- src/app/movi/page.tsx
+++ src/app/movi/page.tsx
@@ -1,11 +1,34 @@
 // TODO: Luninha -> Iniciar tela de Movi
 
 import styles from "./movi.module.css";
+import { Nav } from '../components/nav';
+import { SideBar } from '../components/side-bar';
 
-export default function Skills() {
+export default function Movi() {
     return (
+            <div className="global">
+                <Nav/>
+                <SideBar/>
 
-        <h1>Movimentação</h1>
+                <main className={styles.container}>
+                    <h1 className={styles.card}></h1>
 
+                    <div className={styles.cardGrid}>
+                        <div className={styles.card}>
+                            <h2>POP SWING</h2>
+                            <p>Pop Swing, você continua se movimentando para tirar a precisão de mira do seu oponente.</p>
+                        </div>
+                    <div className={styles.card}>
+                        <h2>AD AD (A DEAD)</h2>
+                        <p>Movimentação Lateral "Para um lado e para o outro" para dificultar o tiro do inimigo.</p>
+                        </div>
+                        <div className={styles.card}>
+                            <h2>STRAFING</h2>
+                            <p>Se mover lateralmente, mantendo um ritmo e precisão em seus movimentos.</p>
+                        </div>
+                    </div>
+                </main>
+        
+            </div>
         );
 }
